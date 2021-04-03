7f45b6c0622b6b0438e01e934609d1694df7dc53
cmajorb <cmajorb@gmail.com>
2021-04-03 12:15:25 -0700

    glasses fix

--- glasses.cpp
+++ glasses.cpp
@@ -15,25 +15,33 @@ int main() {
     cin >> num1;
     cin >> num2;
     cin >> out;
+    bool isValid = false;
     int count = 0;
-    int carry = 0;
-    int i = n-1;
-    while(i >= 0) {
-      int x = (num1[i] - '0')+ (num2[i] - '0') + carry;
-      //cout << "X: " << x << "  " << x%10 << "  " << (int)out[i];
-      if(x%10 != (out[i]- '0')) {
-        count++;
-        num1.erase(i,1);
-        num2.erase(i,1);
-        out.erase(i,1);
-        n--;
-        i = n-1;
-        carry = 0;
-      } else {
-        carry = x/10;
-        i--;
+
+    while(!isValid) {
+      int carry = 0;
+      int i = n-1;
+      while(i >= 0) {
+        int x = (num1[i] - '0')+ (num2[i] - '0') + carry;
+        //cout << "X: " << x << "  " << x%10 << "  " << (int)out[i];
+        if(x%10 != (out[i]- '0')) {
+          count++;
+          num1.erase(i,1);
+          num2.erase(i,1);
+          out.erase(i,1);
+          n--;
+          i = n-1;
+          carry = 0;
+        } else {
+          carry = x/10;
+          i--;
+        }
+      }
+      if(stoi(num1)+stoi(num2) == stoi(out)) {
+        isValid = true;
       }
     }
+
     cout << count;
     return 0;
 }
