4297deed570c85e6618868c9971218031d4acd2b
cmajorb <cmajorb@gmail.com>
2021-04-03 12:02:41 -0700

    glasses

--- glasses.cpp
+++ glasses.cpp
@@ -1,10 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
 
 int main() {
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    string num1,num2,out;
+    cin >> n;
+    cin >> num1;
+    cin >> num2;
+    cin >> out;
+    int count = 0;
+    int carry = 0;
+    int i = n-1;
+    while(i >= 0) {
+      int x = (num1[i] - '0')+ (num2[i] - '0') + carry;
+      //cout << "X: " << x << "  " << x%10 << "  " << (int)out[i];
+      if(x%10 != (out[i]- '0')) {
+        count++;
+        num1.erase(i,1);
+        num2.erase(i,1);
+        out.erase(i,1);
+        n--;
+        i = n-1;
+        carry = 0;
+      } else {
+        carry = x/10;
+        i--;
+      }
+    }
+    cout << count;
     return 0;
 }
