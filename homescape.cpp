d42e63c72aaf43ca39df468226211b37a3a833a6
Benjamin Brownlee <benjamin.brownlee1@gmail.com>
2021-04-03 11:05:01 -0700

    >:(

--- homescape.cpp
+++ homescape.cpp
@@ -1,10 +1,47 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+enum Type {
+    COIN,
+    DIAMOND
+};
 
 int main() {
+    const int MAXVAL = 1000000;
 
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int n, c, d, i;
+    char temp;
+    int cost[MAXVAL];
+    int beauty[MAXVAL];
+    int kind[MAXVAL];
+
+    cin >> n;
+    cin >> c;
+    cin >> d;
+
+    for (i = 0; i < n; i++) {
+        cin >> cost[i];
+        cin >> beauty[i];
+        cin >> temp;
+
+        cout << temp << endl;
+
+        if (temp == 'C')  {
+            kind[i] = 0;
+        }
+        else {
+            kind[i] = 1;
+        }
+    }
+
+    /*
+    for (i = 0; i < n; i++) {
+        for (ii = 0; i 
+        */
+
     return 0;
 }
